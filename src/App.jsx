59547a8238082658a3bc6065817731faa3aeae9b
Faraaz Khan <faraazmohdkhan@gmail.com>
2020-08-23 02:26:30 +0530

    First commit setting layout

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sideform from './components/sideform';
+
+function App() {
+  return (
+    <>
+ <div id="wrapper">
+      <div className="content-page">
+        <div className="content">
+
+
+          <div className="container-fluid">
+
+
+            <div className="row">
+              <div className="col-12">
+
+                <h4 className="page-title">Starter</h4>
+
+                <div className="row">
+                  <Sideform />
+
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
+          </div>
+    </>
+  );
+}
+
+export default App;
