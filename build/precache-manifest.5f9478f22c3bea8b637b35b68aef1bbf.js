5d3a3eb227eaea13005bc7a9c1e7418b6c41e117
Semanticbits <faraazkhan@SemanticbitsMBP.Dlink>
2020-10-14 00:47:23 +0530

    build completed

---
+++ build/precache-manifest.5f9478f22c3bea8b637b35b68aef1bbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129425d26cba41880fa27876965d7a36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37edc6a59dec110ee3d6",
+    "url": "/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "ea67e2dbfda8085dc1d5",
+    "url": "/static/css/main.28920064.chunk.css"
+  },
+  {
+    "revision": "37edc6a59dec110ee3d6",
+    "url": "/static/js/2.1b3ee535.chunk.js"
+  },
+  {
+    "revision": "e94e8792871c09cf72871cffeb41cc1c",
+    "url": "/static/js/2.1b3ee535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea67e2dbfda8085dc1d5",
+    "url": "/static/js/main.0a2fa572.chunk.js"
+  },
+  {
+    "revision": "261b564b9ce0ed36d0d5",
+    "url": "/static/js/runtime-main.576d1f7c.js"
+  },
+  {
+    "revision": "25c22252a4bedeccecb729d75fda050b",
+    "url": "/static/media/bg_naks.25c22252.jpg"
+  }
+]);
